9d84ac88642e78ef2549110ce9435c1de24c1103
BlueTree242 <yahiamsm2@gmail.com>
2023-03-04 11:28:01 +0200

    use google analytics in bundled js

--- fronted/src/main.tsx
+++ fronted/src/main.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Analytics from 'analytics'
+import googleAnalytics from "@analytics/google-analytics";
+
+
+const analytics = Analytics({
+  app: 'awesome-app',
+  plugins: [
+    googleAnalytics({
+      measurementIds: ['G-99S6J5MHBJ']
+    })
+  ]
+})
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
